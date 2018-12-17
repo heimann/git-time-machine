98e525a8ab9d80c40bce7050a96982bc4f21d44d
David Heimann <davidheimann@gmail.com>
2018-12-17 13:02:59 -0500

    Add search bar for git repo and style it

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+  <div class="hello" @click="hideResults">
+    <h1>Git Time Machine</h1>
+    <input type="text"
+           id="repo"
+           name="repo"
+           minlength="4"
+           maxlength="40"
+           size="10"
+           v-model="search"
+           @input="onChange"/>
+    <ul 
+      class="results"
+      v-show="isOpen"
+    >
+      <li v-for="(result, i) in results" :key="i" class="result">
+        {{ result }}
+      </li>
     </ul>
   </div>
 </template>
@@ -34,8 +24,35 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    items: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      search: '',
+      results: [],
+      isOpen: false,
+    };
+  },
+  methods: {
+    onChange() { 
+      if (this.search == '') {
+        this.isOpen = false
+      } else {
+        this.isOpen = true;
+        this.filterResults();
+      }
+    },
+    filterResults() {
+      this.results = this.items.filter(item => item.toLowerCase().indexOf(this.search.toLowerCase()) > -1);
+    },
+    hideResults() {
+      this.isOpen = false;
+    }
+  },
 }
 </script>
 
@@ -44,15 +61,46 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
+
+.hello {
+  margin-top: 30vh;
+}
+
+#repo {
+  width: 550px;
+  height: 40px;
+  font-size: 1em;
+  font-weight: 400;
+  border: 1px solid #bababa;
+  box-sizing: border-box;
+  padding: 0 12px;
+  -webkit-box-shadow: 5px 5x 17px 0px rgba(224,224,224,0.76);
+  -moz-box-shadow: 5px 5px 17px 0px rgba(224,224,224,0.76);
+  box-shadow: 5px 5px 17px 0px rgba(224,224,224,0.76);
+}
+
+#repo:hover {
+  -webkit-box-shadow: 5px 5px 17px 0px rgba(224,224,224,1);
+  -moz-box-shadow: 5px 5px 17px 0px rgba(224,224,224,1);
+  box-shadow: 5px 5px 17px 0px rgba(224,224,224,1);
+}
+
+#repo:focus {
+  outline: none;
+  -webkit-box-shadow: 5px 5px 17px 0px rgba(224,224,224,1);
+  -moz-box-shadow: 5px 5px 17px 0px rgba(224,224,224,1);
+  box-shadow: none;
+}
+.results {
+  box-sizing: border-box;
+  width: 550px;
+  border: 1px solid #bababa;
+  display: block;
+  margin: 0 auto;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  z-index: 1;
+  margin-top: -1px;
+  position: relative;
+  list-style: none;
 }
 </style>
